9c1460949e576d07fb967967ea91a89693c53221
Yanis Saadi <yanis.saadi03@gmail.com>
2026-03-26 16:47:01 +0100

    gros push web

--- eventhubapp/admin.py
+++ eventhubapp/admin.py
@@ -4,4 +4,3 @@ from .models import Event, Participant, Registration
 admin.site.register(Event)
 admin.site.register(Participant)
 admin.site.register(Registration)
-
--- eventhubapp/views.py
+++ eventhubapp/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import Event, Participant, Registration
+from .serializers import EventSerializer, ParticipantSerializer, RegistrationSerializer
 
-# Create your views here.
+
+class IsAdminOrReadOnly(permissions.BasePermission):
+    """Admin/éditeur peut tout faire, viewer a accès en lecture seule."""
+    def has_permission(self, request, view):
+        if request.method in permissions.SAFE_METHODS:  # GET, HEAD, OPTIONS
+            return request.user.is_authenticated
+        return request.user.is_authenticated and request.user.is_staff
+
+
+class EventViewSet(viewsets.ModelViewSet):
+    queryset = Event.objects.all()
+    serializer_class = EventSerializer
+    permission_classes = [IsAdminOrReadOnly]
+
+    def get_queryset(self):
+        queryset = Event.objects.all()
+        # Filtrage par date et statut
+        date = self.request.query_params.get('date')
+        status = self.request.query_params.get('status')
+        if date:
+            queryset = queryset.filter(date__date=date)
+        if status:
+            queryset = queryset.filter(status=status)
+        return queryset
+
+
+class ParticipantViewSet(viewsets.ModelViewSet):
+    queryset = Participant.objects.all()
+    serializer_class = ParticipantSerializer
+    permission_classes = [IsAdminOrReadOnly]
+
+
+class RegistrationViewSet(viewsets.ModelViewSet):
+    queryset = Registration.objects.all()
+    serializer_class = RegistrationSerializer
+    permission_classes = [IsAdminOrReadOnly]
